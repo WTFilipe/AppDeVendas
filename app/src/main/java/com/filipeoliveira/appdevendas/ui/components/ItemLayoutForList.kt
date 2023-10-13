29fdd7d5a5ce2c1c1f01b7fdefd7eaf9e540a3de
Filipe Oliveira <filipe.oliveira@hurb.com>
2023-10-13 20:17:10 -0300

    Adicionando lógica ao botão "Adicionar ao carrinho"

--- app/src/main/java/com/filipeoliveira/appdevendas/ui/components/ItemLayoutForList.kt
+++ app/src/main/java/com/filipeoliveira/appdevendas/ui/components/ItemLayoutForList.kt
@@ -1,10 +1,12 @@
 package com.filipeoliveira.appdevendas.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,15 +14,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
+import com.filipeoliveira.appdevendas.R
 import com.filipeoliveira.appdevendas.data.model.Item
+import com.filipeoliveira.appdevendas.ui.dimen12Dp
 import com.filipeoliveira.appdevendas.ui.dimen150Dp
 import com.filipeoliveira.appdevendas.ui.dimen20Dp
 import com.filipeoliveira.appdevendas.ui.dimen8Dp
@@ -43,7 +54,7 @@ fun ItemLayoutForList(
             modifier = Modifier
         ) {
             ItemLayoutTop(item)
-            ItemLayoutForItemBottom()
+            ItemLayoutBottom()
         }
     }
 }
@@ -105,25 +116,97 @@ private fun ItemLayoutTopRight(item: Item, modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun ItemLayoutForItemBottom(modifier: Modifier = Modifier) {
+private fun ItemLayoutBottom(modifier: Modifier = Modifier) {
+    var selectedQuantity by rememberSaveable {
+        mutableIntStateOf(0)
+    }
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .padding(dimen12Dp)
             .background(MaterialTheme.colorScheme.surfaceVariant),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ItemLayoutBottomLeft(
+            onAddToCartClicked = { selectedQuantity += 1 },
+            modifier = Modifier.weight(1F)
+        )
+        if (selectedQuantity > 0){
+            ItemLayoutBottomRight(
+                modifier = Modifier.weight(1F),
+                selectedQuantity = selectedQuantity,
+                onAddItemClicked = { selectedQuantity += 1 },
+                onRemoveItemClicked = { selectedQuantity -= 1 }
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun ItemLayoutBottomLeft(
+    onAddToCartClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxHeight()
+            .clickable {
+                onAddToCartClicked()
+            },
+        verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
         Text(
             text = "Adicionar ao carrinho",
-            modifier = Modifier
-                .padding(vertical = dimen8Dp),
             textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.labelLarge
+            style = MaterialTheme.typography.labelLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+    }
+}
+
+@Composable
+private fun ItemLayoutBottomRight(
+    modifier: Modifier = Modifier,
+    selectedQuantity: Int,
+    onAddItemClicked: () -> Unit,
+    onRemoveItemClicked: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxHeight(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(dimen8Dp, Alignment.CenterHorizontally)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.icon_minus_circle),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier.clickable {
+                onRemoveItemClicked()
+            }
+        )
+        Text(
+            text = selectedQuantity.toString(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Icon(
+            painter = painterResource(R.drawable.icon_add_circle),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier.clickable {
+                onAddItemClicked()
+            }
         )
     }
 
 }
 
-@Preview
+@Preview(heightDp = 250)
 @Composable
 fun ItemLayoutForListPreview() {
     ItemLayoutForList(
