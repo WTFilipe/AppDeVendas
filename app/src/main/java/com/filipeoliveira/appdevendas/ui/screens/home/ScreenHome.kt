3c59872b4cb78085832be0bc3fd506b982fff309
Filipe Oliveira <filipe.oliveira@hurb.com>
2023-10-15 18:10:32 -0300

    Adicionando estados de erro e carregamento para a ScreenHome

--- app/src/main/java/com/filipeoliveira/appdevendas/ui/screens/home/ScreenHome.kt
+++ app/src/main/java/com/filipeoliveira/appdevendas/ui/screens/home/ScreenHome.kt
@@ -2,6 +2,7 @@ package com.filipeoliveira.appdevendas.ui.screens.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -11,19 +12,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.filipeoliveira.appdevendas.R
 import com.filipeoliveira.appdevendas.data.model.AvailableItem
+import com.filipeoliveira.appdevendas.domain.errors.EmptyResponseError
 import com.filipeoliveira.appdevendas.ui.components.CartResume
+import com.filipeoliveira.appdevendas.ui.components.Loading
 import com.filipeoliveira.appdevendas.ui.dimen16Dp
 import java.math.BigDecimal
 
@@ -54,17 +59,24 @@ private fun ScreenContent(
         verticalArrangement = Arrangement.SpaceBetween,
         modifier = modifier.fillMaxSize()
     ) {
-
-        if (uiState.availableAvailableItemList.isNotEmpty()) {
-            OnAvailableItemsSuccess(
-                data = uiState,
-                modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1F),
-                onAddToCart = { item, selectedQuantity ->
-                    homeViewModel.addToCart(item, selectedQuantity)
-                }
-            )
+        when {
+            uiState.isLoading -> {
+                Loading()
+            }
+            uiState.error != null ->{
+                OnError(uiState.error)
+            }
+            uiState.availableAvailableItemList.isNotEmpty() -> {
+                OnAvailableItemsSuccess(
+                    data = uiState,
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .weight(1F),
+                    onAddToCart = { item, selectedQuantity ->
+                        homeViewModel.addToCart(item, selectedQuantity)
+                    }
+                )
+            }
         }
 
         if (uiState.cartSize > 0){
@@ -121,6 +133,26 @@ fun OnAvailableItemsSuccess(
                 onAddToCart(item,  selectedQuantity)
             },
             selectedQuantity = alreadySelectedQuantity
+        )
+    }
+}
+
+@Composable
+fun OnError(error: Throwable, modifier: Modifier = Modifier) {
+    val errorText = when(error){
+        is EmptyResponseError -> stringResource(R.string.error_empty_response)
+        else -> stringResource(R.string.error_generic)
+    }
+
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+    ){
+        Text(
+            text = errorText,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground,
         )
     }
 }
